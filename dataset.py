6f6be5f1a54543de3dcb81cecd5705f027a93f10
Vaibhav Rastogi <rastogi04vaibhav@gmail.com>
2025-11-09 18:14:53 +0530

    integrated using cached indices

--- dataset.py
+++ dataset.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 from typing import Optional, Tuple, List
 
+import pickle
 import torch
 from decord import VideoReader
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from utils import get_classes
 from enum import Enum
+from black.rusty import Err
 
 
 class FrameSampling(Enum):
@@ -15,101 +17,101 @@ class FrameSampling(Enum):
     PIXEL_INTENSITY = 2
 
 
-def _calculate_likelihood(
-    frame1_chunks: torch.Tensor, frame2_chunks: torch.Tensor
-) -> torch.Tensor:
-    frame1_chunks = frame1_chunks.float()
-    frame2_chunks = frame2_chunks.float()
+# def _calculate_likelihood(
+#     frame1_chunks: torch.Tensor, frame2_chunks: torch.Tensor
+# ) -> torch.Tensor:
+#     frame1_chunks = frame1_chunks.float()
+#     frame2_chunks = frame2_chunks.float()
 
-    # m_i and m_i+1 represents
-    # mean intensity values for a given colour channel for the
-    # region in two consecutive frames
-    m_1 = frame1_chunks.mean(dim=(1, 2))
-    m_2 = frame2_chunks.mean(dim=(1, 2))
+#     # m_i and m_i+1 represents
+#     # mean intensity values for a given colour channel for the
+#     # region in two consecutive frames
+#     m_1 = frame1_chunks.mean(dim=(1, 2))
+#     m_2 = frame2_chunks.mean(dim=(1, 2))
 
-    # s_i and s_i+1 represents
-    # corresponding variance values for a given colour channel for the
-    # region in two consecutive frames
-    s_1 = frame1_chunks.var(dim=(1, 2))
-    s_2 = frame2_chunks.var(dim=(1, 2))
+#     # s_i and s_i+1 represents
+#     # corresponding variance values for a given colour channel for the
+#     # region in two consecutive frames
+#     s_1 = frame1_chunks.var(dim=(1, 2))
+#     s_2 = frame2_chunks.var(dim=(1, 2))
 
-    # handle zero variance
-    # "If s_i = 0 or s_{i+1} = 0 then set s_i = s_{i+1} = 1"
-    # We set any 0 variance to 1 to avoid division by zero.
-    s_1[s_1 == 0] = 1
-    s_2[s_2 == 0] = 1
+#     # handle zero variance
+#     # "If s_i = 0 or s_{i+1} = 0 then set s_i = s_{i+1} = 1"
+#     # We set any 0 variance to 1 to avoid division by zero.
+#     s_1[s_1 == 0] = 1
+#     s_2[s_2 == 0] = 1
 
-    # calculate likelihood ratio components
-    a = (s_1 + s_2) / 2
-    b = ((m_1 - m_2) / 2) ** 2
-    c = (a + b) ** 2
-    d = s_1 * s_2
+#     # calculate likelihood ratio components
+#     a = (s_1 + s_2) / 2
+#     b = ((m_1 - m_2) / 2) ** 2
+#     c = (a + b) ** 2
+#     d = s_1 * s_2
 
-    # Final likelihood ratio per region
-    likelihood_ratio = c / d
+#     # Final likelihood ratio per region
+#     likelihood_ratio = c / d
 
-    # average across the 3 color channels
-    avg_likelihood_per_region = likelihood_ratio.mean(dim=1)
+#     # average across the 3 color channels
+#     avg_likelihood_per_region = likelihood_ratio.mean(dim=1)
 
-    return avg_likelihood_per_region
+#     return avg_likelihood_per_region
 
 
-def _pixel_intensity_sampling(c, vr: VideoReader) -> List[int]:
-    # this function handles frames in `HWC` format
-    # for our use case it would become (224, 224, 3) which is the numpy expected format
-    # however, since we use torch we need our frames in `CHW` format
-    # convert everything to tensors and then permute in the `CHW` format
+# def _pixel_intensity_sampling(c, vr: VideoReader) -> List[int]:
+#     # this function handles frames in `HWC` format
+#     # for our use case it would become (224, 224, 3) which is the numpy expected format
+#     # however, since we use torch we need our frames in `CHW` format
+#     # convert everything to tensors and then permute in the `CHW` format
 
-    nrows, ncols = 4, 4
-    side_of_region = 56  # (width * height)  / num_of_regions -> (224 * 224) / 16
+#     nrows, ncols = 4, 4
+#     side_of_region = 56  # (width * height)  / num_of_regions -> (224 * 224) / 16
 
-    frame_prev = torch.from_numpy(vr[0].asnumpy())
-    total_frames = len(vr)
+#     frame_prev = torch.from_numpy(vr[0].asnumpy())
+#     total_frames = len(vr)
 
-    frame_scores = []
+#     frame_scores = []
 
-    for i in range(1, total_frames):
-        # shape -> (224, 224, 3) -> (HWC)
-        frame_curr = torch.from_numpy(vr[i].asnumpy())
+#     for i in range(1, total_frames):
+#         # shape -> (224, 224, 3) -> (HWC)
+#         frame_curr = torch.from_numpy(vr[i].asnumpy())
 
-        frame1_chunks = (
-            frame_prev.reshape(nrows, side_of_region, ncols, side_of_region, 3)
-            .permute(0, 2, 1, 3, 4)
-            .reshape(-1, side_of_region, side_of_region, 3)
-        )
+#         frame1_chunks = (
+#             frame_prev.reshape(nrows, side_of_region, ncols, side_of_region, 3)
+#             .permute(0, 2, 1, 3, 4)
+#             .reshape(-1, side_of_region, side_of_region, 3)
+#         )
 
-        frame2_chunks = (
-            frame_curr.reshape(nrows, side_of_region, ncols, side_of_region, 3)
-            .permute(0, 2, 1, 3, 4)
-            .reshape(-1, side_of_region, side_of_region, 3)
-        )
+#         frame2_chunks = (
+#             frame_curr.reshape(nrows, side_of_region, ncols, side_of_region, 3)
+#             .permute(0, 2, 1, 3, 4)
+#             .reshape(-1, side_of_region, side_of_region, 3)
+#         )
 
-        likelihood_scores_per_region = _calculate_likelihood(
-            frame1_chunks, frame2_chunks
-        )
+#         likelihood_scores_per_region = _calculate_likelihood(
+#             frame1_chunks, frame2_chunks
+#         )
 
-        total_frame_change_score = likelihood_scores_per_region.mean()
+#         total_frame_change_score = likelihood_scores_per_region.mean()
 
-        frame_scores.append((total_frame_change_score, i))
+#         frame_scores.append((total_frame_change_score, i))
 
-        frame_prev = frame_curr
+#         frame_prev = frame_curr
 
-    sorted_frames = sorted(frame_scores, key=lambda x: x[0], reverse=True)
+#     sorted_frames = sorted(frame_scores, key=lambda x: x[0], reverse=True)
 
-    # Get the indices of the top N-1 frames with the most change
-    # We subtract 1 because we will always add frame 0 by default
-    top_indices = [idx for score, idx in sorted_frames[: c.NUM_FRAMES - 1]]
+#     # Get the indices of the top N-1 frames with the most change
+#     # We subtract 1 because we will always add frame 0 by default
+#     top_indices = [idx for score, idx in sorted_frames[: c.NUM_FRAMES - 1]]
 
-    # Add frame 0 and sort the final indices
-    final_indices = sorted(list(set([0] + top_indices)))
+#     # Add frame 0 and sort the final indices
+#     final_indices = sorted(list(set([0] + top_indices)))
 
-    # Ensure exactly NUM_FRAMES frames
-    if len(final_indices) > c.NUM_FRAMES:
-        final_indices = final_indices[: c.NUM_FRAMES]
-    elif len(final_indices) < c.NUM_FRAMES:
-        final_indices += [final_indices[-1]] * (c.NUM_FRAMES - len(final_indices))
+#     # Ensure exactly NUM_FRAMES frames
+#     if len(final_indices) > c.NUM_FRAMES:
+#         final_indices = final_indices[: c.NUM_FRAMES]
+#     elif len(final_indices) < c.NUM_FRAMES:
+#         final_indices += [final_indices[-1]] * (c.NUM_FRAMES - len(final_indices))
 
-    return final_indices
+#     return final_indices
 
 
 # def _pixel_intensity_sampling(c, vr: VideoReader) -> List[int]:
@@ -183,11 +185,22 @@ class CricketEC(Dataset):
         self.sampling = sampling
         self.config = c
 
+        self.index_map = None
+        cache_path = Path(
+            f"CricketEC-train-test-val/pixel_intensity_indices_{
+                self.config.NUM_FRAMES}_frames.pkl"
+        )
+        if self.sampling == FrameSampling.PIXEL_INTENSITY:
+            print("Loaded indices:", str(cache_path))
+            with open(cache_path, "rb") as f:
+                self.index_map = pickle.load(f)
+
     def __len__(self) -> int:
         return len(self.video_paths)
 
     def load_video_frames(self, idx: int) -> torch.Tensor:
         vr = VideoReader(str(self.video_paths[idx]))
+        video_path = self.video_paths[idx]
 
         indices = []
         match self.sampling:
@@ -198,7 +211,33 @@ class CricketEC(Dataset):
             case FrameSampling.JITTERED:
                 raise NotImplementedError
             case FrameSampling.PIXEL_INTENSITY:
-                indices = _pixel_intensity_sampling(self.config, vr)
+                if self.index_map is None:
+                    raise FileNotFoundError("INDEX MAP NOT LOADING")
+
+                key = str(video_path.relative_to(self.dataset_dir.parent)).replace(
+                    "\\", "/"
+                )
+
+                if key not in self.index_map:
+                    raise KeyError(
+                        f"Video {key} not found in pre-computed index map.")
+
+                indices = self.index_map[key]
+
+                if len(indices) != self.config.NUM_FRAMES:
+                    print(
+                        f"Warning: Mismatch in {key}. "
+                        f"Cache has {len(indices)} frames, config needs {
+                            self.config.NUM_FRAMES}. "
+                        "You may need to re-run pre-computation."
+                    )
+
+                    if len(indices) > self.config.NUM_FRAMES:
+                        indices = indices[: self.config.NUM_FRAMES]
+                    elif len(indices) < self.config.NUM_FRAMES:
+                        indices += [indices[-1]] * (
+                            self.config.NUM_FRAMES - len(indices)
+                        )
 
         frames = torch.from_numpy(vr.get_batch(indices=indices).asnumpy()).permute(
             0, 3, 1, 2
